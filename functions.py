e77e008502cad26879c8c80b5ce60e6cc0a82f67
Shunsuke Saito <shunsuke15vb@gmail.com>
2017-12-05 23:05:21 +0900

    confirming commit 一応、main.py functions.py data.json全てにおいて、やりたいことができる状況にはなった、 あとは、ソート、検索、その他の機能を追加して行けば完成 [task] 追加の機能を入れる それに応じて、jsonDataから適した情報だけを入れるdata変数を作る

---
+++ functions.py
@@ -0,0 +1,37 @@
+import json, datetime
+
+# parameter
+# subject: str // 教科名
+# assignment: str // 課題名
+# deadline: int // 提出期限
+# function: input Jsondata to data.json file
+def inputJson(subject, assignment, deadline):
+    dict = {
+        'kadai1':{
+            'subject':subject,
+            'assignment':assignment,
+            'deadline':deadline
+        }
+    }
+    li = list()
+    with open('data.json', 'r') as target:
+        li = json.load(target)
+        li.append(dict)
+    with open('data.json', 'w') as target:
+        json.dump(li, target, indent=4)
+
+
+# function: read json file and write the data into jsonData
+def outputJson():
+    f = open('data.json', 'r')
+    jsonData = json.load(f)
+    print(jsonData)
+    f.close()
+    return jsonData
+
+# function: return day, month in order
+def get_date():
+    day = datetime.date.today().day
+    month = datetime.date.today().month
+    
+    return day, month
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from functions import get_date
-day, month = get_date()
+import functions
+day, month = functions.get_date()
 print("今日は" + str(month) + "月" + str(day) + "日です。")
 
-fout = open("data.txt", "r")
 print("\n~書き込み済みの課題一覧~")
-print(fout.read())
-fout.close()
+try:
+    jsonData = functions.outputJson()
+except Exception:
+    pass
+####################################
+# ここから
+# listとdictionaryでgetしたJsonを簡単に扱ってみた
+# お試し程度なので、参考ほどに、
+command = int(input('どの教科の課題を確認しますか？\n0:数学...'))
+if command == 0:
+    target = 'math'
 
+for data in jsonData:
+    if data['kadai1']['subject'] == target:
+        print(data['kadai1']['deadline'])
 
-dic_subject = {}
-
+# ここまで
+###################################
 while 1:
     subject = str(input("教科名:"))
     assignment = str(input("課題名:"))
     deadline = int(input("締め切り(mmdd):"))
-    dic_subject[subject] = assignment
-    fout = open("data.txt", "a")
-    fout.write(subject + " " + dic_subject[subject] + " " +str(deadline) )
-    fout.write("\n")
-    fout.close()
+    functions.inputJson(subject, assignment, deadline)
     process = int(input("0 for quit"))
     if process == 0:
         break
