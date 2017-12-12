680d4351c48de3447bb2cb9472db4899d9fa7579
Shunsuke Saito <shunsuke15vb@gmail.com>
2017-12-12 14:42:40 +0900

    開始した時点で、jsonを読み込み、締め切りに応じた処理をする printout_deadline()関数を作成 dueTime引数を追加->締め切り時間にも対応 その他、軽微な修正を行なった

--- functions.py
+++ functions.py
@@ -1,17 +1,18 @@
-import json, datetime
+import json, datetime, math
 
 # parameter
 # subject: str // 教科名
 # assignment: str // 課題名
 # deadline: int // 提出期限
 # function: input Jsondata to data.json file
-def inputJson(subject, assignment, deadline, state):
+def inputJson(subject, assignment, deadline, state, dueTime):
     dict = {
-        'kadai1':{
+        'kadai':{
             'subject':subject,
             'assignment':assignment,
             'deadline':deadline,
-            'state':state
+            'state':state,
+            'dueTime': dueTime
         }
     }
     li = list()
@@ -34,13 +35,57 @@ def outputJson():
 def get_date():
     day = datetime.date.today().day
     month = datetime.date.today().month
-    
     return day, month
 
-# parameter:
-# date: date type (mmdd)
-# return -> day, month
-def parse_date(date):
-    day = date % 100
-    month = date / 100
-    return day, month
+# function: return year
+def get_year():
+    year = datetime.date.today().year
+    return year
+
+def check_date(date):
+    year = int (date / 10000)
+    if(year % 400 == 0):
+        leapyear = True
+    elif(year % 100 == 0):
+        leapyear = False
+    elif(year % 4 == 0):
+        leapyear = True
+    else :
+        leapyear = False
+    mmdd = date % 10000
+    day = mmdd % 100
+    month = int (mmdd / 100)
+    if month < 1 or month >= 13:
+        print("正しい月を入力して下さい\n")
+        return False
+    elif month == 1 or month ==  3 or month == 5 or month ==  7 or month == 8 or month == 10 or month == 12:
+        if day < 1 or day > 31:
+            print("正しい日を入力して下さい\n")
+            return False
+    elif month == 4 or month == 6 or month == 9 or month ==11:
+        if day < 1 or day > 30:
+            print("正しい日を入力して下さい\n")
+            return False
+    elif month == 2:
+        if leapyear:
+            if day < 1 or day > 29:
+                print("(閏年です)正しい日を入力して下さい\n")
+                return False
+        else:
+            if day < 1 or day > 28:
+                print("(閏年ではありません)正しい日を入力して下さい\n")
+                return False
+    return True
+
+# parameter 
+# deadline: yyyymmdd == Integer
+# dueTime: dueTime == String
+# function: print out deadline in an arrangemnt
+def printout_deadline(deadline, dueTime):
+    year = int (deadline / 10000)
+    month = int (deadline % 10000 / 100)
+    day = int (deadline % 10000 % 100)
+    hour = int (dueTime / 100)
+    minute = int (dueTime % 100)
+    print("締め切り日 : " + str(year) + " / " + str(month) + " / " + str(day))
+    print("締め切り時間 : " + "{0:02d}".format(hour) + ":" + "{0:02d}".format(minute))
--- main.py
+++ main.py
@@ -1,28 +1,47 @@
-import functions
+import functions, json
 day, month = functions.get_date()
+year = functions.get_year()
 print("今日は" + str(month) + "月" + str(day) + "日です。")
 
-print("\n~書き込み済みの課題一覧~")
 try:
+    li = list()
+    jsonData = functions.outputJson()
+    for data in jsonData:
+        if data['kadai']['subject'] == "ダミー":
+            continue
+        elif int (data['kadai']['deadline'] / 10000) >= year and month <= int (data['kadai']['deadline'] % 10000 / 100) and day <= int (data['kadai']['deadline'] % 10000 % 100):
+            li.append(data)
+    with open('data.json', 'w') as target:
+        json.dump(li, target, indent=4)
     jsonData = functions.outputJson()
 except Exception:
+    dict = {
+        'kadai': {
+            'subject':"ダミー",
+            'assignment':"00000",
+            'deadline':00000,
+            'state':"00000000",
+            'dueTime':2460
+        }
+    }
+    li = list()
+    with open('data.json', 'r') as target:
+        li.append(dict)
+    with open('data.json', 'w') as target:
+        json.dump(li, target, indent=4)
+    jsonData = functions.outputJson()
     pass
-####################################
-# ここから
-# listとdictionaryでgetしたJsonを簡単に扱ってみた
-# お試し程度なので、参考ほどに、
-# try_command = int(input('どの教科の課題を確認しますか？\n0:数学...'))
-# if command == 0:
-#     target = 'math'
 
-# for data in jsonData:
-#     if data['kadai1']['subject'] == target:
-#         print(data['kadai1']['deadline'])
+print("\n~書き込み済みの課題一覧~")
+for data in jsonData:
+    print("教科名 : " + data['kadai']['subject'])
+    print("課題名 : "  + data['kadai']['assignment'])
+    functions.printout_deadline(data['kadai']['deadline'], data['kadai']['dueTime'])
+    print("[詳細]\n" + data['kadai']['state'] + "\n")
 
-# ここまで
-###################################
 while 1:
-    command = int(input("コマンドを入力してください\n0: 終了\n1: 書き込み\n2: 教科名検索\n3: 締め切り順ソート\n4: 消去\n"))
+    command = int(input("コマンドを入力してください\n0: 終了\n1: 書き込み\n2: 教科名検索\n3: 締め切り順ソート\n"))
+    jsonData = functions.outputJson()
     if command == 0:
         print("終了します")
         break
@@ -30,26 +49,30 @@ while 1:
         print("書き込み")
         subject = str(input("教科名を入力してください"))
         assignment = str(input("課題を入力してください"))
-        deadline = int(input("締め切りを入力してください"))
-        state = int(input("状態を入力してください"))
-        functions.inputJson(subject, assignment, deadline, state)
+        deadline = int(input("締め切りを入力してください(yyyymmdd)"))
+        # 入力されたdeadlineが正しい形式か確かめる
+        if functions.check_date(deadline) is False:
+            continue
+        dueTime = int(input("締め切り時間を入力して下さい(hhmm)"))
+        state = str(input("詳細を入力してください"))
+        functions.inputJson(subject, assignment, deadline, state, dueTime)
     elif command == 2:
         print("教科名検索を行います")
         subject = str(input("教科名を入力してください"))
         for data in jsonData:
-            if data['kadai1']['subject'] == subject:
-                assignment = data['kadai1']['assignment']
-                deadline = data['kadai1']['deadline']
-                print("\n課題名 : " + assignment)
-                print("締め切り : " + str(deadline))
+            if data['kadai']['subject'] == subject:
+                print("\n課題名 : " + str(data['kadai']['assignment']))
+                functions.printout_deadline(data['kadai']['deadline'], data['kadai']['dueTime'])
     elif command == 3:
         print("締め切り順ソート")
-        all_data = sorted(jsonData, key=lambda x:x['kadai1']['deadline'])
+        jsonData = functions.outputJson()
+        all_data = sorted(jsonData, key=lambda x:x['kadai']['deadline'])
         for data in all_data:
-            print("\n教科名 : " + data['kadai1']['subject'])
-            print("課題名 : " + data['kadai1']['assignment'])
-            print("締め切り : " + str(data['kadai1']['deadline']))
-    elif command == 4:
-        print("消去")
+            if data['kadai']['subject'] == 'ダミー':
+                continue
+            print("\n教科名 : " + data['kadai']['subject'])
+            print("課題名 : " + data['kadai']['assignment'])
+            functions.printout_deadline(data['kadai']['deadline'], data['kadai']['dueTime'])
+            print("[詳細]\n" + data['kadai']['state'])
     else:
         print("正しいコマンドを入力してください")
